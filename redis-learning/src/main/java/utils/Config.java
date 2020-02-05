90dd5c28f910acf2c667296e61ca8863088a071e
wanglei <295628068@qq.com>
2020-02-05 18:00:29 +0800

    配置文件更新

--- redis-learning/src/main/java/utils/Config.java
+++ redis-learning/src/main/java/utils/Config.java
@@ -1,9 +1,12 @@
 package utils;
 
 /**
- * 自定义配置文件
+ * 自定义 Redis 配置文件
  */
 public class Config {
-
-    public static String RedisConfig = "redis://pwd654321@127.0.0.1:6379";
+    public static final String REDIS_HOST = "127.0.0.1";    // Redis Host
+    public static final String REDIS_PORT = "6379";         // Redis Port
+    public static final String REDIS_AUTH = "pwd654321";    // Redis Password
+    // 包含 Redis 协议的连接字符串（Lettuce 使用）
+    public static final String REDISCONFIG_PROTOCOL = "redis://pwd654321@127.0.0.1:6379";
 }
