ba59dc74e04b58b02401f895313b90f23dbc5672
wanglei <295628068@qq.com>
2020-02-05 18:08:49 +0800

    修改 port 参数的数据类型

--- redis-learning/src/main/java/utils/Config.java
+++ redis-learning/src/main/java/utils/Config.java
@@ -5,7 +5,7 @@ package utils;
  */
 public class Config {
     public static final String REDIS_HOST = "127.0.0.1";    // Redis Host
-    public static final String REDIS_PORT = "6379";         // Redis Port
+    public static final Integer REDIS_PORT = 6379;         // Redis Port
     public static final String REDIS_AUTH = "pwd654321";    // Redis Password
     // 包含 Redis 协议的连接字符串（Lettuce 使用）
     public static final String REDISCONFIG_PROTOCOL = "redis://pwd654321@127.0.0.1:6379";
