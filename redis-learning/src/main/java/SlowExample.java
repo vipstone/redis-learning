777765ee50fca941a028138f7ffe4be3129bc5ad
wanglei <295628068@qq.com>
2020-02-18 12:05:54 +0800

    添加慢查询日志示例

---
+++ redis-learning/src/main/java/SlowExample.java
@@ -0,0 +1,29 @@
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.util.Slowlog;
+import utils.JedisUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 慢查询
+ */
+public class SlowExample {
+    public static void main(String[] args) {
+        Jedis jedis = JedisUtils.getJedis();
+        // 插入慢查询（因为 slowlog-log-slower-than 设置为 0，所有命令都符合慢操作）
+        jedis.set("db", "java");
+        jedis.set("lang", "java");
+        // 慢查询记录的条数
+        long logLen = jedis.slowlogLen();
+        // 所有慢查询
+        List<Slowlog> list = jedis.slowlogGet();
+        // 循环打印
+        for (Slowlog item : list) {
+            System.out.println("慢查询命令："+ Arrays.asList(item.getArgs())+
+                    " 执行了："+item.getExecutionTime()+" 微秒");
+        }
+        // 清空慢查询日志
+        jedis.slowlogReset();
+    }
+}
